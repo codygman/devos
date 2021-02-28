8c4f9225fc3e535f99876b79376bf01e2c1fbe98
codygman <cody@codygman.dev>
2021-02-28 13:35:04 -0600

    comment out broken haskellPackage override
    I think it's broken on unstable for some reason but this code
    probably works on 20.09

--- overrides/default.nix
+++ overrides/default.nix
@@ -19,15 +19,15 @@
       signal-desktop
       starship;
 
-    haskellPackages = prev.haskellPackages.override {
-      overrides = hfinal: hprev:
-        let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-        in
-        {
-          # same for haskell packages, matching ghc versions
-          inherit (pkgs.haskell.packages."ghc${version}")
-            haskell-language-server;
-        };
-    };
+    # haskellPackages = prev.haskellPackages.override {
+    #   overrides = hfinal: hprev:
+    #     let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
+    #     in
+    #     {
+    #       # same for haskell packages, matching ghc versions
+    #       inherit (pkgs.haskell.packages."ghc${version}")
+    #         haskell-language-server;
+    #     };
+    # };
   };
 }
